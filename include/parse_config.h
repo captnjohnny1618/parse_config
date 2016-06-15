445fefdffaf2cc310ffc6b06325b625bbc3df3cb
captnjohnny1618 <jhoffm@vt.edu>
2016-06-15 15:27:38 -0700

    Minor tweaks to cooperate better with CTBB projections

--- include/parse_config.h
+++ include/parse_config.h
@@ -31,6 +31,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #define COMMENT_DELIM '%'
 
--- src/parse_config.c
+++ src/parse_config.c
@@ -25,15 +25,9 @@
    freect.project@gmail.com.*/
 
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
+//#include <string.h>
 
 #include "parse_config.h"
-
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
 size_t find_char(char * str, char delim){
     int i=0;
